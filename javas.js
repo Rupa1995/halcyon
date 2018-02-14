7ab2bf7d858c5138b9a47d6c713900ad0d33994c
rupa <ranirupa47@gmail.com>
2018-02-14 13:37:09 +0530

    14feb scroll

--- javas.js
+++ javas.js
@@ -1,9 +1,58 @@
-function openNav()
-		{
-		  document.getElementById("mynav").style.width = "100%";
-		}
-		function closeNav() {
-		    document.getElementById("mynav").style.width = "0";
-		}
+//==================================== Nav ==================================================//
+		$(document).ready(function() {
+  			$(document).delegate('.open', 'click', function(event){
+   				 $(this).addClass('oppenned');
+    				event.stopPropagation();
 
-		
+ 			 })
+ 			 $(document).delegate('body', 'click', function(event) {
+   				 $('.open').removeClass('oppenned');
+ 			 })
+ 			$(document).delegate('.cls', 'click', function(event){
+   				 $('.open').removeClass('oppenned');
+    				event.stopPropagation();
+  			});
+		});
+
+// ==================================== Scroll to Top ===========================================//
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+//====================================== Smooth Scrolling ========================================//
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
