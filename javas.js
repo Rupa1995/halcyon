f52ef87bad6962cb9a1bc0b48a149bdb9f3c3b08
rupa <ranirupa47@gmail.com>
2018-02-15 15:47:56 +0530

    15feb improved

--- javas.js
+++ javas.js
@@ -9,18 +9,20 @@
 // ==================================== Scroll to Top ===========================================//
 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+	var height = $(this).scrollTop();
+    if (height > 50) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
 });
+$(document).ready(function() {
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
-    }, 500);
+    }, 2000);
 });
-
+});
 //====================================== Smooth Scrolling ========================================//
 $(document).ready(function(){
     $("a").on('click', function(event) {
@@ -29,7 +31,7 @@ $(document).ready(function(){
       		var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 2000, function(){
         window.location.hash = hash;
       });
     } 
