a83a2e5add0f9779a2ce3ab54ffa9a23f8438d14
rupa <ranirupa47@gmail.com>
2018-02-15 13:44:32 +0530

    15feb 2

--- javas.js
+++ javas.js
@@ -10,12 +10,30 @@
 // ==================================== Scroll to Top ===========================================//
 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+	if($(window).width() > 767){
+    if (($(this).scrollTop() >= 50)&($(this).scrollTop() <= 4500)) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
     }
      else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
+}
+else if($(window).width() < 480){
+	    if (($(this).scrollTop() >= 50)&($(this).scrollTop() <= 7800)) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    }
+     else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+}
+else if(($(window).width() > 481)&($(window).width() < 766)){
+	    if (($(this).scrollTop() >= 50)&($(this).scrollTop() <= 12000)) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    }
+     else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+}
 });
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
